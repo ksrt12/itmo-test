4d2202fbc40b0418cf3eafa858bd7b42b6fe3ac0
ksrt12 <ksrt12group@gmail.com>
2021-10-07 23:38:09 +0300

    animate slider
    Signed-off-by: ksrt12 <ksrt12group@gmail.com>

--- src/main/banner/Banner.tsx
+++ src/main/banner/Banner.tsx
@@ -1,9 +1,10 @@
 import bg from "./bg.svg";
 import "./banner.scss";
+import { useState } from "react";
 
 function BannerCard(props: { [key: string]: string; }) {
     return (
-        <div className="banner-card">
+        <div className="banner-card" style={{ width: props.cardWidth }}>
             <h1>{props.title}</h1>
             <p>{props.text}</p>
             <a className="click-more" href={props.href}>Подробнее</a>
@@ -12,29 +13,53 @@ function BannerCard(props: { [key: string]: string; }) {
 }
 
 function Banner() {
+    const [slideIndex, setSlideIndex] = useState(0);
 
     const cards = [
-        // {
-        //     title: "Слайдер 2",
-        //     text: "Программа предполагает углубленное изучение вопросов, связанных с проектированием, исследованием, производством и эксплуатацией мехатронных и робототехнических систем и комплексов.",
-        //     href: "#"
-        // },
         {
             title: "Началась подача документов",
             text: "Программа предполагает углубленное изучение вопросов, связанных с проектированием, исследованием, производством и эксплуатацией мехатронных и робототехнических систем и комплексов.",
             href: "#"
-        }
+        },
+        {
+            title: "Слайд 2",
+            text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam quisquam a, qui sequi alias deleniti perspiciatis molestias voluptate inventore ex sapiente recusandae omnis eveniet veniam, earum impedit. Deleniti, ratione eaque?",
+            href: "#"
+        },
+        {
+            title: "Слайд 3",
+            text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam quisquam a, qui sequi alias deleniti perspiciatis molestias voluptate inventore ex sapiente recusandae omnis eveniet veniam, earum impedit. Deleniti, ratione eaque?",
+            href: "#"
+        },
     ];
+    const cardsNum = cards.length;
+    const cardWidth = 100 / cardsNum;
+
+    const changeCard = (next: boolean) => {
+        let activeIndex = slideIndex;
+        if (next) {
+            activeIndex += 1;
+            if (activeIndex === cardsNum) {
+                activeIndex = 0;
+            }
+        } else {
+            activeIndex -= 1;
+            if (activeIndex < 0) {
+                activeIndex = cardsNum - 1;
+            }
+        }
+        setSlideIndex(activeIndex);
+    };
 
     return (
         <div className="banner" style={{ backgroundImage: `url(${bg})` }}>
-            <i className="bi bi-arrow-left-circle"></i>
+            <i className="bi bi-arrow-left-circle" onClick={() => changeCard(false)}></i>
             <div className="banner-cards">
-                <div className="overflow">
-                    {cards.map(card => <BannerCard key={card.title} {...card} />)}
+                <div className="overflow" style={{ width: ` ${cardsNum * 100}%`, transform: `translateX(-${slideIndex * cardWidth}%)` }}>
+                    {cards.map(card => <BannerCard key={card.title} cardWidth={`${cardWidth}%`} {...card} />)}
                 </div>
             </div>
-            <i className="bi bi-arrow-right-circle"></i>
+            <i className="bi bi-arrow-right-circle" onClick={() => changeCard(true)}></i>
         </div>
     );
 
